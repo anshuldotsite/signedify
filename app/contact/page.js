a09be1d1c21f3df5b4250f7cde65c51a92368655
Richard <elitegem84@gmail.com>
2025-06-21 02:22:42 -0400

    change to about section added padding for contact section

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,7 +2,7 @@
 
 export default function Contact() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 pt-[4rem]">
       <div className="bg-white rounded-xl shadow-md p-8 max-w-md w-full">
         <h1 className="text-2xl font-semibold text-blue-800 mb-4 text-center">
           Contact Us
